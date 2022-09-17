52d41fd7a03ceaf172a435b7d83d37614cd80827
jabber <2898117012@qq.com>
2022-09-18 01:34:37 +0800

    init

--- src/RecommenderShortCode.php
+++ src/RecommenderShortCode.php
@@ -33,10 +33,9 @@ class RecommenderShortCode
                 <div class="wd_recommender_list woocommerce">
                     <ul class="products related">
                         <?php
-                        $result = [4722, 4711, 4668, 4667, 4634, 4618];
                         foreach ($result as $item) {
                             $post_object = get_post($item);
-//                            $post_object = get_post($item->product_id);
+                            $post_object = get_post($item->product_id);
                             setup_postdata($GLOBALS['post'] =& $post_object);
                             wc_get_template_part('content', 'product');
                         }
